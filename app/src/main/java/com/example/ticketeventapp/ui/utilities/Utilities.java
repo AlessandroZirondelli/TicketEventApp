da970f3bae2c1c999ff9ac22b1d6939105eb7f17
Alessandro Zirondelli <alessandr.zirondelli@studio.unibo.it>
2022-12-04 17:57:23 +0100

    Fix add back stack double fragment

--- app/src/main/java/com/example/ticketeventapp/ui/utilities/Utilities.java
+++ app/src/main/java/com/example/ticketeventapp/ui/utilities/Utilities.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 import com.example.ticketeventapp.R;
+import com.example.ticketeventapp.ui.home.fragment.HomeFragment;
 import com.example.ticketeventapp.ui.reglog.fragment.LoginFragment;
 
 
@@ -24,7 +25,7 @@ public class Utilities {
     public static void insertHomeFragment(AppCompatActivity activity, Fragment fragment, String tag, int fragmentContainerResId){
         FragmentTransaction transaction = activity.getSupportFragmentManager().beginTransaction();
         transaction.replace(fragmentContainerResId,fragment, tag);
-        transaction.addToBackStack(tag);
+        //transaction.addToBackStack(tag);
         transaction.commit();
     }
 }
