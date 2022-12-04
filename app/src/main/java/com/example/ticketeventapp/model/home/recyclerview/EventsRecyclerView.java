8e7365a78e70b9052d0aed52b8f59fbf365eeaf4
Alessandro Zirondelli <alessandr.zirondelli@studio.unibo.it>
2022-12-04 22:47:45 +0100

    fix error. Test recyclerview

--- app/src/main/java/com/example/ticketeventapp/model/home/recyclerview/EventsRecyclerView.java
+++ app/src/main/java/com/example/ticketeventapp/model/home/recyclerview/EventsRecyclerView.java
@@ -4,6 +4,10 @@ import android.app.Activity;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.ticketeventapp.R;
+import com.example.ticketeventapp.model.mng_events.Event;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class EventsRecyclerView {
     private Activity activity;
@@ -14,11 +18,26 @@ public class EventsRecyclerView {
     }
 
 
-    private void  setRecyclerView(){
+    public void  setRecyclerView(OnItemListener onItemListener){
         eventsRecyclerView = activity.findViewById(R.id.recycler_view_events);
         eventsRecyclerView.setHasFixedSize(true);
         // Here I should set Adapter to pass data to display
-        //EventItemAdapter eventItemAdapter = new EventItemAdapter();
-        //eventsRecyclerView.setAdapter(eventItemAdapter);
+        List<Event> eventList = new ArrayList<>();
+        Event event1 = new Event("Event1","","2020-03-04","15:45","","","add_photo_alternate","","");
+        Event event2 = new Event("Event2","","2022-07-10","20:00","","","add_photo_alternate","","");
+        Event event3 = new Event("Event3","","2022-07-10","20:00","","","add_photo_alternate","","");
+        Event event4 = new Event("Event4","","2022-07-10","20:00","","","add_photo_alternate","","");
+        Event event5 = new Event("Event5","","2022-07-10","20:00","","","add_photo_alternate","","");
+        Event event6 = new Event("Event6","","2022-07-10","20:00","","","add_photo_alternate","","");
+
+        eventList.add(event1);
+        eventList.add(event2);
+        eventList.add(event3);
+        eventList.add(event4);
+        eventList.add(event5);
+        eventList.add(event6);
+
+        EventItemAdapter eventItemAdapter = new EventItemAdapter(activity,eventList,onItemListener);
+        eventsRecyclerView.setAdapter(eventItemAdapter);
     }
 }
--- app/src/main/java/com/example/ticketeventapp/ui/home/fragment/RecyclerViewEventsFragment.java
+++ app/src/main/java/com/example/ticketeventapp/ui/home/fragment/RecyclerViewEventsFragment.java
@@ -1,22 +1,49 @@
 package com.example.ticketeventapp.ui.home.fragment;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import com.example.ticketeventapp.R;
+import com.example.ticketeventapp.model.home.recyclerview.EventsRecyclerView;
+import com.example.ticketeventapp.model.home.recyclerview.OnItemListener;
 
 
-public class RecyclerViewEventsFragment extends Fragment {
+public class RecyclerViewEventsFragment extends Fragment implements OnItemListener {
+
+    private EventsRecyclerView eventsRecyclerView;
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.testhomelist, container, false);
+        return inflater.inflate(R.layout.recycler_view_events_layout, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        eventsRecyclerView = new EventsRecyclerView(getActivity());
+        eventsRecyclerView.setRecyclerView(this);
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onItemClick(int position) {
+        Activity activity = getActivity();
+        if (activity != null){
+            /*Utilities.insertFragment((AppCompatActivity) activity, new DetailsFragment(),
+                    DetailsFragment.class.getSimpleName());*/
+
+            //listViewModel.setItemSelected(adapter.getItemSelected(position));
+            Log.e("HomeFragment","Clickato");
+        }
     }
 }
