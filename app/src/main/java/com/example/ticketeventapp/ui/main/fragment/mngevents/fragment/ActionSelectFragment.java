5b4fd8a304844a5664c3b4ca55a328007106e7fa
Alessandro Zirondelli <alessandr.zirondelli@studio.unibo.it>
2022-12-19 17:47:34 +0100

    SelectedEvent is null when user press back on SelectionAsctionFragment

--- app/src/main/java/com/example/ticketeventapp/ui/main/fragment/mngevents/fragment/ActionSelectFragment.java
+++ app/src/main/java/com/example/ticketeventapp/ui/main/fragment/mngevents/fragment/ActionSelectFragment.java
@@ -3,18 +3,22 @@ package com.example.ticketeventapp.ui.main.fragment.mngevents.fragment;
 import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
+import androidx.activity.OnBackPressedCallback;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.example.ticketeventapp.R;
 import com.example.ticketeventapp.ui.utilities.Utilities;
+import com.example.ticketeventapp.viewmodel.mng_events.EventListViewModel;
 
 public class ActionSelectFragment extends Fragment {
 
@@ -22,11 +26,29 @@ public class ActionSelectFragment extends Fragment {
     private ImageView scan;
     private ImageView joiners;
     private ImageView modify;
+    private EventListViewModel eventListViewModel;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_action_select, container, false);
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        eventListViewModel =  new ViewModelProvider(getActivity()).get(EventListViewModel.class);
+
+        OnBackPressedCallback callback = new OnBackPressedCallback(true /* enabled by default */) {
+            @Override
+            public void handleOnBackPressed() {
+                // Handle the back button event
+                Log.e("HomeFrament","Turn indrì action select");
+                eventListViewModel.setSelectedEventItem(null);
+                getFragmentManager().popBackStack();
+            }
+        };
+        requireActivity().getOnBackPressedDispatcher().addCallback(this, callback);
     }
 
     @Override
