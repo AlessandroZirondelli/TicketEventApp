7c3aafd643f64ce7e9712509663c3b694ba1643c
Alessandro Zirondelli <alessandr.zirondelli@studio.unibo.it>
2022-12-22 15:49:23 +0100

    Load qrcode image drawable without Picasso

--- app/src/main/java/com/example/ticketeventapp/model/home/recyclerview/adapter/TicketItemAdapter.java
+++ app/src/main/java/com/example/ticketeventapp/model/home/recyclerview/adapter/TicketItemAdapter.java
@@ -1,11 +1,14 @@
 package com.example.ticketeventapp.model.home.recyclerview.adapter;
 
 import android.app.Activity;
+import android.graphics.drawable.Drawable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.content.res.AppCompatResources;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelStoreOwner;
 import androidx.recyclerview.widget.DiffUtil;
@@ -19,6 +22,7 @@ import com.example.ticketeventapp.model.home.recyclerview.viewholder.TicketViewH
 import com.example.ticketeventapp.model.mng_events.Event;
 import com.example.ticketeventapp.model.mng_tickets.Ticket;
 import com.example.ticketeventapp.viewmodel.mng_tickets.TicketListViewModel;
+import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
@@ -53,7 +57,27 @@ public class TicketItemAdapter extends RecyclerView.Adapter<TicketViewHolder>{
         holder.set_event_name_text("TestName");
         holder.set_event_date_text("TestDate");
         holder.set_event_time_text("TestTime");
-        Picasso.get().load(R.drawable.qr_code_2).into(holder.qrcode);
+        /*if(holder.qrcode!=null){
+            Log.e("Bug","Holder qrcode NOT null");
+        }
+
+
+        Picasso.get().load(R.drawable.calendar_month).placeholder(R.drawable.qr_code_2).into(holder.qrcode, new Callback() {
+            @Override
+            public void onSuccess() {
+                Log.e("Bug","Loaded image ok");
+            }
+
+            @Override
+            public void onError(Exception e) {
+
+                Log.e("Bug",e.toString());
+            }
+        });
+        Drawable drawable;*/
+
+        Drawable drawable = AppCompatResources.getDrawable(activity, activity.getResources().getIdentifier("@drawable/qr_code_2","drawable",activity.getPackageName()));
+        holder.set_event_photo_drawable(drawable);
 
     }
 
