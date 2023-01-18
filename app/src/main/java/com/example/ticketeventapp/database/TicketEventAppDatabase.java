f1ec234a2dcd94ade6a7eff408b1d5b9174fbb4f
Alessandro Zirondelli <alessandr.zirondelli@studio.unibo.it>
2023-01-18 19:08:56 +0100

    Clean code TicketEventAppDatabase

--- app/src/main/java/com/example/ticketeventapp/database/TicketEventAppDatabase.java
+++ app/src/main/java/com/example/ticketeventapp/database/TicketEventAppDatabase.java
@@ -14,24 +14,15 @@ import com.example.ticketeventapp.model.mng_users.User;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-/*Qui dobbiamo andare a specificare tutte le entities(tabelle) che sono presenti nel database. Nel nostro caso sula una. Dobbiamo esplicitarla come classe.*/
 @Database(entities = {User.class, Event.class, Ticket.class}, version = 1)
 public abstract class TicketEventAppDatabase extends RoomDatabase {
 
-    /* Dobbiamo rendere pubbliche in maniera astratta le classi DAO che sono associate alle varie entities !!*/
-
     public abstract TicketEventAppDAO ticketEventAppDAO();
-
-    //definisco una istanza singleton unica del database per tutto il progetto
     private static volatile TicketEventAppDatabase INSTANCE;
-
-    //è necessaria un pool di thread per evitare che si blocchi la GUI
-    //creo 4 thread, perchè è un numero "buono" per la gestione del DB e raccomandato da Android
     static final ExecutorService executor = Executors.newFixedThreadPool(4);
 
     static TicketEventAppDatabase getDatabase(final Context context) {
         if(INSTANCE == null){
-            //syncronized ci permette di avere sinronizzato con il singleton
             synchronized (TicketEventAppDatabase.class){
                 if(INSTANCE == null){ // ce lo richiediamo perchè eseguendo thread diversi potrebbe essere successo qualcosa nel mentre
                     INSTANCE = Room.databaseBuilder(context.getApplicationContext(),TicketEventAppDatabase.class,
